855e2b998a33fc1a40e8183680790281b2154463
Fahim Razzaq <famraz@gmail.com>
2023-08-21 17:47:16 +0100

    changes

--- app/controllers/instruments_controller.rb
+++ app/controllers/instruments_controller.rb
@@ -1,9 +1,23 @@
 class InstrumentsController < ApplicationController
   def index
-    @istruments = Instrument.all
+    @instruments = Instrument.all
   end
 
   def show
     @instruments.find(params[:id])
   end
+
+  def new
+    @instruments = Restaurant.new
+  end
+
+  def create
+    @instruments = Instrument.new(instruments_params)
+    if @instruments.save
+      redirect_to instruments_path(@instruments)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
 end
