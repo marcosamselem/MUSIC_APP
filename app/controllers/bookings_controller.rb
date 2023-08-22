c7ea50e2b9dd450b23c0c6db94f2816b74990ea8
Marcos Amselem <marcosamselem27@gmail.com>
2023-08-22 17:34:59 +0100

    boking controller finished

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,29 @@
 class BookingsController < ApplicationController
+  def new
+    @booking = Booking.new
+    @instrument = Instrument.find(params[:instrument_id])
+  end
+
+  def create
+    @booking = Booking.new(booking_params)
+    @instrument = Instrument.find(params[:instrument_id])
+    @booking.instrument_id = @instrument.id
+    @booking.user_id = current_user.id
+  if @booking.save
+      redirect_to confirmation_booking_path(@booking)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def confirmation
+    @booking = Booking.find(params[:id])
+  end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:start, :end)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
-  resources :instruments, only: [:index, :show, :new, :create, :destroy]
-
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  resources :instruments, only: [:index, :show, :new, :create, :destroy] do
+    resources :bookings, only: [:create, :new]
+  end
+  resources :bookings, only: [] do
+    member do
+      get :confirmation
+    end
+    end
 end
