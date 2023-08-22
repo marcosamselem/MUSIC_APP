353edeeb20afa536cf4d1eff81516ece23179df3
Fahim Razzaq <famraz@gmail.com>
2023-08-22 07:02:45 +0100

    new and create methods

--- app/controllers/instruments_controller.rb
+++ app/controllers/instruments_controller.rb
@@ -1,9 +1,22 @@
 class InstrumentsController < ApplicationController
   def index
-    @istruments = Instrument.all
+    @instruments = Instrument.all
   end
 
   def show
-    @instruments.find(params[:id])
+    @instrument.find(params[:id])
+  end
+
+  def new
+    @instrument = Instrument.new
+  end
+
+  def create
+    @instrument = instrument.new(instrument_params)
+    if @instrument.save
+      redirect_to instrument_path(@instrument)
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 end
