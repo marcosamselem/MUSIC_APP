68914a3ab33cd283e82429cda99ee17b2eae9b61
Fahim Razzaq <famraz@gmail.com>
2023-08-22 12:12:28 +0100

    new and create controller action

--- app/controllers/instruments_controller.rb
+++ app/controllers/instruments_controller.rb
@@ -1,15 +1,20 @@
 class InstrumentsController < ApplicationController
-
   def new
     @instrument = Instrument.new
   end
 
   def create
-    @instrument = instrument.new(instrument_params)
+    @instrument = Instrument.new(instrument_params)
     if @instrument.save
       redirect_to instrument_path(@instrument)
     else
       render :new, status: :unprocessable_entity
     end
+  end
+
+  private
+
+  def instrument_params
+    params.require(:instrument).permit(:name, :price, :location)
   end
 end
