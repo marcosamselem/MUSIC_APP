ab11e7228bae32a453abbf8993e76f3a2d644c73
Marcos Amselem <marcosamselem27@gmail.com>
2023-08-25 11:36:49 +0100

    last changes

--- app/models/instrument.rb
+++ app/models/instrument.rb
@@ -2,6 +2,7 @@ class Instrument < ApplicationRecord
   belongs_to :user
   has_many :bookings
   has_one_attached :photo
+  validates :photo, presence: true
   geocoded_by :location
   after_validation :geocode, if: :will_save_change_to_location?
 end
